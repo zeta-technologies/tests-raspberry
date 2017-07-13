5900a9cc0141a15202d10659d7b2cba16c0c330c
JoSchmutz <jonathanschmutz0@gmail.com>
2017-07-13 16:17:29 +0200

    modified --forces-yes to -y

--- openbciSetup.sh
+++ openbciSetup.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-sudo apt update --forces -yes
-sudo apt full-upgrade --forces-yes
-wget --quiet -O - https://deb.nodesource.com/setup_8.x --forces-yes | sudo -E bash - --forces-yes
-sudo apt install nodejs --forces-yes
-sudo apt-get install npm --forces-yes
-npm install openbci-ganglion lodash clone --forces-yes
-sudo apt-get install Bluetooth bluez-utils blueman bluez python-gobject python-gobject-2
-sudo apt-get install libbluetooth-dev
-sudo apt-get install python-numpy python-scipy python-matplotlib ipython ipython-notebook python-pandas python-sympy python-nose --forces-yes
-sudo apt-get install python-pyaudio
+sudo apt -y update
+sudo apt -y full-upgrade
+wget --quiet -O - https://deb.nodesource.com/setup_8.x | sudo -E bash -
+sudo apt -y install nodejs
+sudo apt-get -y install npm
+sudo apt-get -y install Bluetooth bluez-utils blueman bluez python-gobject python-gobject-2
+sudo apt-get -y install libbluetooth-dev
+sudo apt-get -y install python-numpy python-scipy python-matplotlib python-pandas python-sympy python-nose
+sudo apt-get -y install python-pyaudio
+npm install openbci-ganglion
+npm install lodash
+npm install clone
