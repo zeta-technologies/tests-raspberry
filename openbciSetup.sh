2bca508ba06c11f2394dbd313949f2220b3e64be
JoSchmutz <jonathanschmutz0@gmail.com>
2017-07-13 12:22:35 +0200

    automate setup

---
+++ openbciSetup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cd Desktop
+git clone https://github.com/zeta-technologies/tests-raspberry
+cd tests-raspberry
+sudo apt update && sudo apt full-upgrade
+wget --quiet -O - https://deb.nodesource.com/setup_8.x | sudo -E bash -
+sudo apt install nodejs
+sudo apt-get install npm
+npm install openbci-ganglion lodash clone
+sudo apt-get install Bluetooth bluez-utils blueman
+sudo apt-get install python-gobject python-gobject-2 libbluetooth-dev python-numpy python-scipy python-matplotlib ipython ipython-notebook python-pandas python-sympy python-nose python-pyaudio
