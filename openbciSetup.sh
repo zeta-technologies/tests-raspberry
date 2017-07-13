9e95f095d447f2dd7e74a151f2a98268afbac1f4
JoSchmutz <jonathanschmutz0@gmail.com>
2017-07-13 15:48:18 +0200

    added --forces-yes

--- openbciSetup.sh
+++ openbciSetup.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-sudo apt update && sudo apt full-upgrade
-wget --quiet -O - https://deb.nodesource.com/setup_8.x | sudo -E bash -
-sudo apt install nodejs
-sudo apt-get install npm
-npm install openbci-ganglion lodash clone
-sudo apt-get install Bluetooth bluez-utils blueman
-sudo apt-get install python-gobject python-gobject-2 libbluetooth-dev python-numpy python-scipy python-matplotlib ipython ipython-notebook python-pandas python-sympy python-nose python-pyaudio
+sudo apt update --forces -yes
+sudo apt full-upgrade --forces-yes
+wget --quiet -O - https://deb.nodesource.com/setup_8.x --forces-yes | sudo -E bash - --forces-yes
+sudo apt install nodejs --forces-yes
+sudo apt-get install npm --forces-yes
+npm install openbci-ganglion lodash clone --forces-yes
+sudo apt-get install Bluetooth bluez-utils blueman bluez python-gobject python-gobject-2
+sudo apt-get install libbluetooth-dev
+sudo apt-get install python-numpy python-scipy python-matplotlib ipython ipython-notebook python-pandas python-sympy python-nose --forces-yes
+sudo apt-get install python-pyaudio
