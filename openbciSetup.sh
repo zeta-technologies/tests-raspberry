c12d42ed6f464b98d5a3f9c059813ce00141738c
JoSchmutz <jonathanschmutz0@gmail.com>
2017-07-19 09:28:35 +0200

    deleted python-sympy

--- openbciSetup.sh
+++ openbciSetup.sh
@@ -5,8 +5,8 @@ sudo apt-get -y purge wolfram-engine
 sudo apt-get -y purge libreoffice
 sudo aptitude hold wolfram-engine
 sudo aptitude hold libreoffice
-sudo apt update
-sudo apt -y full-upgrade
+# sudo apt update
+# sudo apt -y full-upgrade
 wget --quiet -O - https://deb.nodesource.com/setup_8.x | sudo -E bash -
 sudo apt -y install nodejs
 sudo apt-get -y install npm
@@ -21,6 +21,7 @@ sudo apt-get -y install python-pandas
 # sudo apt-get -y install python-sympy
 # sudo apt-get -y install python-nose
 sudo apt-get -y install python-pyaudio
+
 npm install openbci-ganglion
 npm install lodash
 npm install clone
