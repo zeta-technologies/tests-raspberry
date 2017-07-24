95ba6d9673388c0b6c124ec4fef042a30ca5449b
JoSchmutz <jonathanschmutz0@gmail.com>
2017-07-24 18:04:53 +0200

    duration session set to 30s and 430s

--- constantes_PunchinBall.py
+++ constantes_PunchinBall.py
@@ -93,9 +93,9 @@ veryoldPosy = maxDisplayY
 timer = ['images/0.png', 'images/1.png', 'images/2.png', 'images/3.png', 'images/4.png', 'images/5.png',
                     'images/6.png', 'images/7.png', 'images/8.png', 'images/9.png']
 restingState = 'images/restingState.png'
-restingStateDuration = 430 # in seconds
+restingStateDuration = 30 # in seconds
 sec = 0
-durationSessionInit =
+durationSessionInit =  430
 durationSession = durationSessionInit
 
 '''Navigation among the pages'''
