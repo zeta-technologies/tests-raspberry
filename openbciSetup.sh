f04da39773b86e557f0cd6cfa7bbd01cdb5586bb
JoSchmutz <jonathan.schmutz@polytechnique.edu>
2017-10-17 10:30:42 +0200

    up to date setup, date is set to 2017-10-10

--- openbciSetup.sh
+++ openbciSetup.sh
@@ -1,27 +1,25 @@
 #!/bin/bash
-sudo apt-get clean
-sudo apt-get autoremove
+sudo date --set 2017-10-10
+curl -sL https://deb.nodesource/setup_8.x | sudo -E bash -
+sudo apt install nodejs
+
 sudo apt-get -y purge wolfram-engine
 sudo apt-get -y purge libreoffice
 sudo aptitude hold wolfram-engine
 sudo aptitude hold libreoffice
 # sudo apt update
 # sudo apt -y full-upgrade
-wget --quiet -O - https://deb.nodesource.com/setup_8.x | sudo -E bash -
-sudo apt -y install nodejs
-sudo apt-get -y install npm
 sudo apt-get -y install Bluetooth
-sudo apt-get -y install bluez-utils
 sudo apt-get -y install blueman
 sudo apt-get -y install bluez
 sudo apt-get -y install libbluetooth-dev
-sudo apt-get -y install python-numpy
-sudo apt-get -y install python-scipy
-sudo apt-get -y install python-pandas
-# sudo apt-get -y install python-sympy
-# sudo apt-get -y install python-nose
-sudo apt-get -y install python-pyaudio
-
-npm install openbci-ganglion
+sudo apt-get -y install python-numpy python-scipy python-pandas python-pyaudio
+sudo pip install colour
+cd ~/Desktop
+mkdir v011
+cd v011
+git clone -b v011 https://github.com/zeta-technologies/tests-raspberry
+cd tests-raspberry/OBGanglion
+npm install
 npm install lodash
 npm install clone
