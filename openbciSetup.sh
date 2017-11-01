46d5231544cb1d7163291d1874597878c3415c8b
JoSchmutz <jonathanschmutz0@gmail.com>
2017-11-01 15:56:52 +0100

    cleaned file

--- openbciSetup.sh
+++ openbciSetup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo date --set 2017-10-10
+sudo date --set 2017-11-01
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 sudo apt -y install nodejs
 sudo apt-get -y install sshpass
@@ -7,20 +7,16 @@ sudo apt-get -y purge wolfram-engine
 sudo apt-get -y purge libreoffice
 sudo aptitude -y hold wolfram-engine
 sudo aptitude -y hold libreoffice
-# sudo apt update
-# sudo apt -y full-upgrade
-
 sudo apt-get -y install Bluetooth
 sudo apt-get -y install blueman
 sudo apt-get -y install bluez
 sudo apt-get -y install libbluetooth-dev
-sudo apt-get -y install python-numpy python-scipy python-pandas python-pyaudio
+sudo apt-get -y install python-numpy
+sudo apt-get -y install python-scipy
+sudo apt-get -y install python-pandas
+sudo apt-get -y install python-pyaudio
 sudo pip install colour
-cd ~/Desktop
-mkdir v011
-cd v011
-git clone -b v011 https://github.com/zeta-technologies/tests-raspberry
-cd tests-raspberry/OBGanglion
-npm install
+cd ~/Desktop/v011/tests-raspberry/OBGanglion
 npm install lodash
 npm install clone
+npm install
