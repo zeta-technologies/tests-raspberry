584393ad77302df822be1bd5113d27d146c3a973
JoSchmutz <jonathanschmutz0@gmail.com>
2017-11-02 11:42:04 +0100

    set duration to 3minutes

--- constantes.py
+++ constantes.py
@@ -85,11 +85,11 @@ veryOldPosy = maxDisplayY
 timer = ['images/0.png', 'images/1.png', 'images/2.png', 'images/3.png', 'images/4.png', 'images/5.png',
                     'images/6.png', 'images/7.png', 'images/8.png', 'images/9.png']
 restingState = 'images/restingState.png'
-restingStateDuration = 30 # in seconds
+restingStateDuration = 180 # in seconds
 sec = 0
 secRS2 = 0
 secRS1 = 0
-durationSessionInit =  350
+durationSessionInit =  180
 durationSession = durationSessionInit
 restingState1isDone = 0
 endSessionImg = 'images/endSession.png'
